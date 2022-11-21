4a5167155a7d5342526800b9aac463a48abc4a25
Thomas NICOLAS <tom.nicolas@protonmail.com>
2022-11-21 12:10:43 +0100

    paddle mouvement

--- pyBricks.py
+++ pyBricks.py
@@ -1,4 +1,9 @@
+# small bricks breaker inb Python 3 for Openclassrooms project
+# by Tom NICOLAS
+
 import turtle
+
+# Window Setup
 
 window = turtle.Screen()
 window.title('Casse-Brique by Tom Nicolas')
@@ -6,5 +11,57 @@ window.bgcolor('black')
 window.setup(width=800, height=600)
 window.tracer(0)
 
+# Score
+
+
+
+# Level
+
+
+
+# Bricks
+
+
+
+# Paddle
+
+paddle = turtle.Turtle()
+paddle.speed(0)
+paddle.shape('square')
+paddle.shapesize(stretch_len=5)
+paddle.color('white')
+paddle.penup()
+paddle.goto(0,-250)
+
+# Ball
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('circle')
+ball.shapesize(stretch_len=0.5, stretch_wid=0.5)
+ball.color('white')
+ball.penup()
+ball.goto(0,0)
+
+# Functions 
+
+def paddle_left():
+    x = paddle.xcor()
+    x += -20
+    paddle.setx(x)
+
+def paddle_right():
+    x = paddle.xcor()
+    x += 20
+    paddle.setx(x)
+
+
+# Keyboard binding
+
+window.listen()
+window.onkeypress(paddle_left, 'Left')
+window.onkeypress(paddle_right, 'Right')
+
+# Main Lopp
 while True:
     window.update()
