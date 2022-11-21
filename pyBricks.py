c44be70d7601130e85fa5cecd43e43a2456fd19f
Thomas NICOLAS <tom.nicolas@protonmail.com>
2022-11-21 21:02:23 +0100

    press space to start + restart

--- pyBricks.py
+++ pyBricks.py
@@ -1,4 +1,4 @@
-# small bricks breaker inb Python 3 for Openclassrooms project
+# small bricks breaker in Python 3 for Openclassrooms project
 # by Tom NICOLAS
 
 import turtle
@@ -7,10 +7,21 @@ import random
 # Window Setup
 
 wn = turtle.Screen()
-wn.title('Casse-Brique by Tom Nicolas')
+wn.title('-- Bricks Breaker -- by Tom Nicolas')
 wn.bgcolor('black')
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+
+# Paddle
+
+paddle = turtle.Turtle()
+paddle.speed(0)
+paddle.shape('square')
+paddle.shapesize(stretch_len=5, stretch_wid=0.5)
+paddle.color('white')
+paddle.penup()
+paddle.goto(0,-250)
 
 # Ball
 
@@ -20,7 +31,7 @@ ball.shape('circle')
 ball.shapesize(stretch_len=0.5, stretch_wid=0.5)
 ball.color('white')
 ball.penup()
-ball.goto(0,-200)
+ball.goto(0,-239)
 ball.setheading(random.randint(20,160))
 ballspeed = 0.04
 
@@ -43,72 +54,83 @@ startmessage.shape("blank")
 startmessage.penup()
 startmessage.setposition(0,25)
 startmessage.pendown()
-startmessage.write("", align = "center", font = ("Fira code", 12))
-
+startmessage.write("Press SPACE To Start", align = "center", font = ("Fira code", 12))
 
 # Bricks
 
 
 
-# Paddle
-
-paddle = turtle.Turtle()
-paddle.speed(0)
-paddle.shape('square')
-paddle.shapesize(stretch_len=5, stretch_wid=0.5)
-paddle.color('white')
-paddle.penup()
-paddle.goto(0,-250)
 
 
 # Functions 
 
 def paddle_left():
-    x = paddle.xcor()
-    x += -20
-    paddle.setx(x)
+    if paddle.xcor() > -350:
+        x = paddle.xcor()
+        x += -10
+        paddle.setx(x)
 
 def paddle_right():
-    x = paddle.xcor()
-    x += 20
-    paddle.setx(x)
+    if paddle.xcor() < 350:
+        x = paddle.xcor()
+        x += 10
+        paddle.setx(x)
+
+gameContinue = False
+
+def start_game():
+    global gameContinue
+    gameContinue = True
+
+def restart_game():
+    ball.goto(paddle.xcor(),(paddle.ycor() +10))
+    ball.setheading(random.randint(20,160))
+    global gameContinue
+    gameContinue = False
 
 
-
-
-""" def start_game():
-    global running
-    running = True
-    ball_start() """
 
 
 # Keyboard binding
 
-wn.listen()
 wn.onkeypress(paddle_left, 'Left')
 wn.onkeypress(paddle_right, 'Right')
-#wn.onkeypress(start_game, 'space')
+wn.onkeypress(start_game, 'space')
+wn.listen()
 
-# Main Lopp
+#move_one_step()
+wn.update()
+
+# Main Lopp  
 while True:
+
     wn.update()
-    ball.forward(ballspeed)
-    print(ball.xcor(),ball.ycor())
+    #ball.forward(ballspeed)
 
-
+    if gameContinue:
+        ball.forward(ballspeed)
+        startmessage.clear()
+    
     # Borders
+
     if ball.ycor() > 295:
         ball.setheading(360 - ball.heading())
-
     
     if ball.ycor() < -300:
         startmessage.write("Game Over", align = "center", font = ("Fira code", 24, 'normal'))
+        gameContinue = False    
+        restart_game()
     
-    
-    if ball.xcor() < -395 or ball.xcor() > 395:
+    if ball.xcor() < -390 or ball.xcor() > 390:
         ball.setheading(180 - ball.heading())
 
     # Paddle and Ball collisions
 
-    if (ball.ycor() < -245 and ball.ycor() > -260) and (ball.xcor() < paddle.xcor() +48 and ball.xcor() > paddle.xcor() -48):
+    if (ball.ycor() < -240 and ball.ycor() > -260) and (ball.xcor() < paddle.xcor() +50 and ball.xcor() > paddle.xcor() +10):
+        ball.setheading(-20 - ball.heading())
+
+    if (ball.ycor() < -240 and ball.ycor() > -260) and (ball.xcor() > paddle.xcor() -50 and ball.xcor() < paddle.xcor() -10):
+        ball.setheading(20 - ball.heading())
+
+    if (ball.ycor() < -240 and ball.ycor() > -260) and (ball.xcor() < paddle.xcor() +10 and ball.xcor() > paddle.xcor() -10):
         ball.setheading(0 - ball.heading())
