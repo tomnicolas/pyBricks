8ff1a95763d7d73618c491882e24575adb8980db
Thomas NICOLAS <tom.nicolas@protonmail.com>
2022-11-21 15:32:03 +0100

    bouncing top and sides

--- pyBricks.py
+++ pyBricks.py
@@ -2,18 +2,35 @@
 # by Tom NICOLAS
 
 import turtle
+import random
 
 # Window Setup
 
-window = turtle.Screen()
-window.title('Casse-Brique by Tom Nicolas')
-window.bgcolor('black')
-window.setup(width=800, height=600)
-window.tracer(0)
+wn = turtle.Screen()
+wn.title('Casse-Brique by Tom Nicolas')
+wn.bgcolor('black')
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Ball
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('circle')
+ball.shapesize(stretch_len=0.5, stretch_wid=0.5)
+ball.color('white')
+ball.penup()
+ball.goto(0,-200)
+ball.setheading(random.randint(20,160))
 
 # Score
 
-
+""" text = turtle.Turtle()
+text.shape('blank')
+text.color('white')
+text.speed(0)
+text.setposition(200, 250)
+text.write('x = {} y = {}'.format(ball.xcor(),ball.ycor()), align = "center", font = ("Courier", 8, "bold")) """
 
 # Level
 
@@ -33,15 +50,6 @@ paddle.color('white')
 paddle.penup()
 paddle.goto(0,-250)
 
-# Ball
-
-ball = turtle.Turtle()
-ball.speed(0)
-ball.shape('circle')
-ball.shapesize(stretch_len=0.5, stretch_wid=0.5)
-ball.color('white')
-ball.penup()
-ball.goto(0,0)
 
 # Functions 
 
@@ -56,12 +64,33 @@ def paddle_right():
     paddle.setx(x)
 
 
+
+
+""" def start_game():
+    global running
+    running = True
+    ball_start() """
+
+
 # Keyboard binding
 
-window.listen()
-window.onkeypress(paddle_left, 'Left')
-window.onkeypress(paddle_right, 'Right')
+wn.listen()
+wn.onkeypress(paddle_left, 'Left')
+wn.onkeypress(paddle_right, 'Right')
+#wn.onkeypress(start_game, 'space')
 
 # Main Lopp
 while True:
-    window.update()
+    wn.update()
+    ball.forward(0.03)
+    print(ball.xcor(),ball.ycor())
+
+
+    # Border
+    if ball.ycor() > 295:
+        ball.setheading(360 - ball.heading())
+    
+    if ball.xcor() < -395 or ball.xcor() > 395:
+        ball.setheading(180 - ball.heading())
+
+    
