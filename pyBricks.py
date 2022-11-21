cd421be21527117dc694286a523baac89e6054f9
Thomas NICOLAS <tom.nicolas@protonmail.com>
2022-11-21 16:08:56 +0100

    bouncing ball on paddle

--- pyBricks.py
+++ pyBricks.py
@@ -22,15 +22,17 @@ ball.color('white')
 ball.penup()
 ball.goto(0,-200)
 ball.setheading(random.randint(20,160))
+ballspeed = 0.04
 
 # Score
-
-""" text = turtle.Turtle()
-text.shape('blank')
-text.color('white')
-text.speed(0)
-text.setposition(200, 250)
-text.write('x = {} y = {}'.format(ball.xcor(),ball.ycor()), align = "center", font = ("Courier", 8, "bold")) """
+startmessage = turtle.Turtle()
+startmessage.speed(0)
+startmessage.color("white")
+startmessage.shape("blank")
+startmessage.penup()
+startmessage.setposition(0,75)
+startmessage.pendown()
+startmessage.write("", align = "center", font = ("Courier", 12))
 
 # Level
 
@@ -45,7 +47,7 @@ text.write('x = {} y = {}'.format(ball.xcor(),ball.ycor()), align = "center", fo
 paddle = turtle.Turtle()
 paddle.speed(0)
 paddle.shape('square')
-paddle.shapesize(stretch_len=5)
+paddle.shapesize(stretch_len=5, stretch_wid=0.5)
 paddle.color('white')
 paddle.penup()
 paddle.goto(0,-250)
@@ -82,15 +84,23 @@ wn.onkeypress(paddle_right, 'Right')
 # Main Lopp
 while True:
     wn.update()
-    ball.forward(0.03)
+    ball.forward(ballspeed)
     print(ball.xcor(),ball.ycor())
 
 
-    # Border
+    # Borders
     if ball.ycor() > 295:
         ball.setheading(360 - ball.heading())
+
+    
+    if ball.ycor() < -300:
+        startmessage.write("Game Over", align = "center", font = ("Courier", 12))
+    
     
     if ball.xcor() < -395 or ball.xcor() > 395:
         ball.setheading(180 - ball.heading())
 
-    
+    # Paddle and Ball collisions
+
+    if ball.ycor() < -245 and (ball.xcor() < paddle.xcor() +48 and ball.xcor() > paddle.xcor() -48):
+        ball.setheading(0 - ball.heading())
