e5d6c8cf11bea1caf85a8682c4e71f511d7d8855
Thomas NICOLAS <tom.nicolas@protonmail.com>
2022-11-21 11:42:37 +0100

    import turtle + screen setup

---
+++ pyBricks.py
@@ -0,0 +1,10 @@
+import turtle
+
+window = turtle.Screen()
+window.title('Casse-Brique by Tom Nicolas')
+window.bgcolor('black')
+window.setup(width=800, height=600)
+window.tracer(0)
+
+while True:
+    window.update()
