ca9d48d6ac603bc299911dd5e376b17077aa068c
Thomas NICOLAS <tom.nicolas@protonmail.com>
2022-11-22 14:18:39 +0100

    V1 fin + readme update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pyBricks
+
+A little Bricks Breaker made with Python Turtle,
+
+to play, just run the .py file.
--- pyBricks.py
+++ pyBricks.py
@@ -14,18 +14,6 @@ wn.setup(width=800, height=600)
 wn.tracer(0)
 
 
-# Paddle
-
-paddle = turtle.Turtle()
-paddle.speed(0)
-paddle.shape('square')
-paddle.shapesize(stretch_len=5, stretch_wid=0.5)
-paddle.color('white')
-paddle.penup()
-paddle.goto(0,-250)
-deltaPaddle = 20
-
-
 # Ball
 
 ball = turtle.Turtle()
@@ -37,6 +25,17 @@ ball.penup()
 ball.goto(0,-239)
 ball.setheading(random.randint(20,160))
 ballspeed = 0.18
+
+# Paddle
+
+paddle = turtle.Turtle()
+paddle.speed(0)
+paddle.shape('square')
+paddle.shapesize(stretch_len=5, stretch_wid=0.5)
+paddle.color('white')
+paddle.penup()
+paddle.goto(0,-250)
+deltaPaddle = 30
 
 # Score
 
@@ -73,8 +72,6 @@ infomessage.pendown()
 infomessage.write("Use the keys <- and -> to move LEFT or RIGHT", align = "center", font = ("Fira code", 12))
 
 # Bricks
-
-
         
 def makeRow(x,y):
     global row
@@ -108,22 +105,25 @@ def collisionBrick(row):
             brick.clear()
             brick.penup()
             brick.goto(1000, 1000)
-            ballspeed = ballspeed + 0.008
+            ballspeed += 0.008
             score += 5
             scoreText.clear()
             scoreText.write("Score = " + str(score), font = ("Fira code", 12))
             wn.update()
             return ballspeed, score
 
+def hideBricks(row):
+    for brick in row:
+        brick.penup()
+        brick.goto(1000, 1000)
 
 def collisionPaddle():
     if (ball.ycor() < -240 and ball.ycor() > -260) and (ball.xcor() < paddle.xcor() +50 and ball.xcor() > paddle.xcor() +10):
         ball.setheading(-20 - ball.heading())
     if (ball.ycor() < -240 and ball.ycor() > -260) and (ball.xcor() > paddle.xcor() -50 and ball.xcor() < paddle.xcor() -10):
         ball.setheading(20 - ball.heading())
-    if (ball.ycor() < -240 and ball.ycor() > -260) and (ball.xcor() < paddle.xcor() +9.9 and ball.xcor() > paddle.xcor() -9.9):
+    if (ball.ycor() < -240 and ball.ycor() > -260) and (ball.xcor() < paddle.xcor() +9.99 and ball.xcor() > paddle.xcor() -9.99):
         ball.setheading(0 - ball.heading())
-
 
 def collisionWall():
     if ball.ycor() > 295:
@@ -133,13 +133,13 @@ def collisionWall():
     if ball.xcor() < -395 or ball.xcor() > 390:
         ball.setheading(180 - ball.heading())
   
-def paddleLeft():
+def paddleMoveLeft():
     if paddle.xcor() > -350:
         x = paddle.xcor()
         x += -deltaPaddle
         paddle.setx(x)
 
-def paddleRight():
+def paddleMoveRight():
     if paddle.xcor() < 340:
         x = paddle.xcor()
         x += deltaPaddle
@@ -152,34 +152,40 @@ def startGame():
     gameContinue = True
 
 def restartGame():
-    global gameContinue, GAME
+    global gameContinue
     gameContinue = False
-    ball.goto(0,-239)
-    paddle.goto(0,-250)
+    paddle.ht()
+    ball.ht()
+    hideBricks(row1)
+    hideBricks(row2)
+    hideBricks(row3)
     ball.setheading(random.randint(20,160))
-    score = 0
     scoreText.clear()
     scoreText.write("Score = " + str(score), font = ("Fira code", 12, 'normal'))
+    startmessage.clear()
     startmessage.penup()
     startmessage.setposition(0,10)
     startmessage.pendown()
-    startmessage.write("GAME OVER,", align = "center", font = ("Fira code", 32, 'normal'))
+    startmessage.write("GAME OVER,", align = "center", font = ("Fira code", 32,'normal'))
+    infomessage.clear()
     infomessage.penup()
     infomessage.setposition(0,-70)
     infomessage.pendown()
-    infomessage.write("press SPACE to Restart,\n\n  press ESC, to Quit.", align = "center", font = ("Fira code", 12,))
+    infomessage.write("press ESC, to Quit.", align = "center", font = ("Fira code", 12,'normal'))
 
 def winGame():
     paddle.ht()
     ball.ht()
+    startmessage.clear()
     startmessage.penup()
     startmessage.setposition(0,0)
     startmessage.pendown()
     startmessage.write("YOU WIN !", align = "center", font = ("Fira code", 32, 'normal'))
+    infomessage.clear()
     infomessage.penup()
     infomessage.setposition(0,-50)
     infomessage.pendown()
-    infomessage.write("press ESC, to Quit.", align = "center", font = ("Fira code", 12,))    
+    infomessage.write("press ESC, to Quit.", align = "center", font = ("Fira code", 12,'normal'))    
 
 def quitGame():
     global GAME
@@ -187,8 +193,8 @@ def quitGame():
 
 # Keyboard binding
 
-wn.onkeypress(paddleLeft, 'Left')
-wn.onkeypress(paddleRight, 'Right')
+wn.onkeypress(paddleMoveLeft, 'Left')
+wn.onkeypress(paddleMoveRight, 'Right')
 wn.onkeypress(startGame, 'space')
 wn.onkeypress(quitGame, 'Escape')
 wn.listen()
@@ -199,13 +205,12 @@ gameContinue = False
 
 
 # Main Loop  
+
 while GAME:
 
     wn.update()
 
-    # Start the Ball
-
-    if gameContinue:
+    if gameContinue:                # Start the Ball
         ball.forward(ballspeed)
         startmessage.clear()
 
@@ -217,4 +222,5 @@ while GAME:
     collisionBrick(row3)
     collisionPaddle()
     collisionWall()
-    
+
+wn.update()
